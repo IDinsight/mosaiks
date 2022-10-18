d102ca4cc0400888010a82def04e4684665fa212
amir_emami <amirali1376@gmail.com>
2022-10-18 16:38:00 +0530

    use SHRUG shapefiles. refactor. addition of grouped stats.

--- notebooks/custom/utils.py
+++ notebooks/custom/utils.py
@@ -5,8 +5,47 @@ import geopandas as gpd
 import pandas as pd
 import numpy as np
 
+from sklearn.metrics import r2_score
+from scipy.stats import pearsonr, kendalltau, spearmanr
 
-def import_shapefiles():
+import seaborn as sns
+
+
+def import_shrug_shapefiles():
+
+    shapefile_path = (
+        Path(__file__).parents[2]
+        / "data"
+        / "SHRUG"
+        / "geometries_shrug-v1.5.samosa-open-polygons-shp"
+        / "village.shp"
+    )
+    shrug_shp = gpd.read_file(shapefile_path)
+
+    shrug_shp.rename(
+        columns={
+            "pc11_s_id": "pc11_state_id",
+            "pc11_d_id": "pc11_district_id",
+            "pc11_sd_id": "pc11_subdistrict_id",
+            "pc11_tv_id": "pc11_village_id",
+        },
+        inplace=True,
+    )
+
+    # change to int (also removes trailing zeros)
+    shrug_shp = shrug_shp.astype(
+        {
+            "pc11_state_id": int,
+            "pc11_district_id": int,
+            "pc11_subdistrict_id": int,
+            "pc11_village_id": int,
+        }
+    )
+
+    return shrug_shp
+
+
+def import_nasa_shapefiles():
     """
     Import shapefiles from the data directory and merge into one GeoDataFrame.
 
@@ -30,6 +69,64 @@ def import_shapefiles():
     dbf = pd.concat(dbf_list, ignore_index=True)
 
     return dbf
+
+
+def import_shrug_r_keys():
+    """
+    Import the SHRUG rural 2011 keys.
+
+    Returns
+    -------
+    pd.DataFrame
+
+    """
+    shrug_r_keys = pd.read_csv(
+        Path(__file__).parents[2]
+        / "data"
+        / "SHRUG"
+        / "shrug-v1.5.samosa-keys-csv"
+        / "shrug_pc11r_key.csv"
+    )
+
+    # drop any entries that are missing IDs
+    shrug_r_key_clean = shrug_r_keys.dropna(
+        subset=[
+            "pc11_state_id",
+            "pc11_district_id",
+            "pc11_subdistrict_id",
+            "pc11_village_id",
+        ]
+    )
+
+    return shrug_r_key_clean
+
+
+def import_shrug_u_keys():
+    """
+    Import the SHRUG urban 2011 keys.
+
+    Returns
+    -------
+    pd.DataFrame
+
+    """
+    shrug_u_keys = pd.read_csv(
+        Path(__file__).parents[2]
+        / "data"
+        / "SHRUG"
+        / "shrug-v1.5.samosa-keys-csv"
+        / "shrug_pc11u_key.csv"
+    )
+
+    # drop any entries that are missing IDs
+    shrug_u_key_clean = shrug_u_keys.dropna(
+        subset=[
+            "pc11_state_id",
+            "pc11_town_id",
+        ]
+    )
+
+    return shrug_u_key_clean
 
 
 def get_bounds(gdf):
@@ -84,10 +181,30 @@ def create_coords_list(min_long, min_lat, max_long, max_lat, step=0.05):
     return coords_list
 
 
-def coords_list_to_gdf(coords_list):
+def show_results(y_test, y_pred):
+    """
+    Print stats and plot true vs predicted values.
 
-    # Convert coords pairs to geopandas points
-    geometry = gpd.points_from_xy(x=coords_list[:, 1], y=coords_list[:, 0])
-    coords_gdf = gpd.GeoDataFrame(geometry=geometry, crs="EPSG:4326")
+    Parameters
+    ----------
+    y_test : array-like
+        The true values
+    y_pred : array-like
+        The predicted values
 
-    return coords_gdf
+    Returns
+    -------
+    None
+
+    """
+    print("r2: %f" % r2_score(y_test, y_pred))
+    print("pearson: %f" % pearsonr(y_test, y_pred)[0])
+    print("kendall: %f" % kendalltau(y_test, y_pred)[0])
+    print("spearman: %f" % spearmanr(y_test, y_pred)[0])
+
+    # scatterplot
+    ax = sns.scatterplot(x=y_pred, y=y_test)
+    sns.lineplot(x=[0, 1], y=[0, 1], color="black", linestyle="--", ax=ax)
+    ax.set(xlabel="Predicted", ylabel="Observed")
+    ax.set_ylim(-0.1, 1.1)
+    ax.set_xlim(-0.2, 1.1)
