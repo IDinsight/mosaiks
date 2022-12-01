d5eec003583232c4513e86cc68ae8303ab3df524
amiraliemami <amirali1376@gmail.com>
2022-12-01 14:01:49 +0000

    script updates

--- src/01_preprocess_create_mosaiks_points.py
+++ src/01_preprocess_create_mosaiks_points.py
@@ -1,3 +1,14 @@
+# TO-DO
+
+# Contents of main() should be a separate function
+
+# Add   
+# 1. path_to_shapes
+# 2. step
+# 3. pre_calc_bounds
+# 4. subset to 6 states var
+# as script arguments
+
 from custom.mosaiks_points import (
     create_gdf_of_enclosed_points,
     plot_selected_points,
@@ -15,21 +26,22 @@ def main():
         "01_preprocessed/SHRUG/shrug_pc11r_key_with_shapes",
         "shrug_pc11r_key_with_shapes.shp",
     )
-    shrug_key_geoms = lengthen_shapefile_ID_names(shrug_key_geoms)
 
     # optional - subset shapes to a smaller area. E.g. to 6 focus states:
-    # shrug_r_matched = shrug_r_matched[shrug_r_matched["pc11_state_id"].isin([8, 16, 18, 20, 22, 23, 28])]
+    # shrug_r_matched = shrug_r_matched[shrug_r_matched["pc11_s_id"].isin([8, 16, 18, 20, 22, 23, 28])]
 
     # create lattice of point coordinates, keeping those that sit within our shrug shapes - takes around 50s
     selected_points_gdf = create_gdf_of_enclosed_points(
         shrug_key_geoms,
         step=0.05,
-        pre_calc_bounds="india",  # remove this if selection is not all-india
+        pre_calc_bounds="india",
     )
 
     # save plot and csv. Saves to "data/01_preprocessed/mosaiks_request_points"
     file_name = "INDIA_SHRUG_request_points"
-    plot_selected_points(selected_points_gdf, file_name=file_name)
+    plot_selected_points(
+        selected_points_gdf, 
+        color_column="pc11_s_id", file_name=file_name)
     points_to_latlon_df(selected_points_gdf.geometry, file_name)
 
 
