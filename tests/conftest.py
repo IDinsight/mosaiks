1740324df6f90649d1a84b84ae754bd2c2dbc297
v_kt_r <entropysmashers@gmail.com>
2023-04-11 16:05:26 +1000

    the load yaml function does not work in other contexts so changing the config call

--- tests/conftest.py
+++ tests/conftest.py
@@ -15,15 +15,36 @@ def sample_test_data():
 
 @pytest.fixture(scope="session")
 def featurization_params():
-    return utl.load_yaml_config("featurisation.yaml")
+    params = {
+        "satellite_search_params": {
+            "satellite_name": "landsat-8-c2-l2",
+            "seasonal": False,
+            "year": 2014,
+            "search_start": "2013-04-01",
+            "search_end": "2014-03-31",
+            "stac_output": "least_cloudy",
+            "stac_api": "planetary-compute",
+        },
+        "num_features": 4000,
+        "kernel_size": 3,
+        "batch_size": 10,
+        "device": "cpu",
+        "dask": {"n_partitions": 200},
+    }
+    return params
 
 
 @pytest.fixture(scope="session")
 def satellite_config(featurization_params):
-    satellite_config = utl.load_yaml_config("satellite_config.yaml")
-    return satellite_config[
-        featurization_params["satellite_search_params"]["satellite_name"]
-    ]
+    satellite_config = {
+        "resolution": 30,
+        "dtype": "int16",
+        "bands": ["SR_B2", "SR_B3", "SR_B4", "SR_B5", "SR_B6", "SR_B7"],
+        "buffer_distance": 1200,
+        "min_image_edge": 30,
+    }
+
+    return satellite_config
 
 
 @pytest.fixture(scope="session")
