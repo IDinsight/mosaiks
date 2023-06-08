8bdd19f7050d44abab7e9e7e5e3489ac7c7ef60b
poornimaramesh <poornimaramesh1995@gmail.com>
2023-06-08 21:33:20 +0100

    added test for stac item fetch from stac id

--- tests/test_stac_fetch.py
+++ tests/test_stac_fetch.py
@@ -5,71 +5,76 @@ import pytest
 from pystac.item import Item
 
 import mosaiks.utils as utl
-from mosaiks.fetch.stacs import fetch_seasonal_stac_items, fetch_stac_items
+from mosaiks.fetch.stacs import (
+    fetch_seasonal_stac_items,
+    fetch_stac_item_from_id,
+    fetch_stac_items,
+    get_stac_api,
+)
 
 
+# -----Tests for stac item fetching-----
 @pytest.fixture(scope="module")
 def test_points_with_stac(sample_test_data: gpd.GeoDataFrame):
-    """Sample test data with stac IDs."""
+    """Sample test data with stac items."""
     points_gdf = utl.df_w_latlons_to_gdf(sample_test_data)
     return fetch_stac_items(
         points_gdf, "landsat-8-c2-l2", "2013-04-01", "2014-03-31", "planetary-compute"
     )
 
 
+def test_if_stac_items_are_added_to_test_df(test_points_with_stac: gpd.GeoDataFrame):
+    assert "stac_item" in test_points_with_stac.columns and type(
+        test_points_with_stac["stac_item"].tolist()[0] == Item
+    )
+
+
+def test_if_df_with_stac_items_has_correct_shape(
+    test_points_with_stac: gpd.GeoDataFrame, sample_test_data: gpd.GeoDataFrame
+):
+    assert len(test_points_with_stac) == len(sample_test_data)
+
+
+# -----Tests for stac item fetching with null data-----
 @pytest.fixture(scope="module")
 def test_points_with_stac_null(sample_test_null_data: gpd.GeoDataFrame):
-    """Sample test data with stac IDs."""
+    """Sample test data with stac items."""
     points_gdf = utl.df_w_latlons_to_gdf(sample_test_null_data)
     return fetch_stac_items(
         points_gdf, "landsat-8-c2-l2", "2013-04-01", "2014-03-31", "planetary-compute"
     )
 
 
-@pytest.fixture(scope="module")
-def test_points_with_seasonal_stac(sample_test_data: gpd.GeoDataFrame):
-    """Sample test data with stac IDs."""
-    points_gdf = utl.df_w_latlons_to_gdf(sample_test_data)
-    return fetch_seasonal_stac_items(
-        points_gdf, "landsat-8-c2-l2", 2013, "planetary-compute"
-    )
-
-
-# -----Tests for stac ID fetching-----
-def test_if_stac_ids_are_added_to_test_df(test_points_with_stac: gpd.GeoDataFrame):
-    assert "stac_item" in test_points_with_stac.columns and type(
-        test_points_with_stac["stac_item"].tolist()[0] == Item
-    )
-
-
-def test_if_df_with_stac_ids_has_correct_shape(
-    test_points_with_stac: gpd.GeoDataFrame, sample_test_data: gpd.GeoDataFrame
-):
-    assert len(test_points_with_stac) == len(sample_test_data)
-
-
-# -----Tests for stac ID fetching with null data-----
-def test_if_stac_ids_are_added_to_test_null_df(
+def test_if_stac_items_are_added_to_test_null_df(
     test_points_with_stac_null: gpd.GeoDataFrame,
 ):
     print(test_points_with_stac_null.columns)
     assert "stac_item" in test_points_with_stac_null.columns
 
 
-def test_if_null_df_with_stac_ids_has_correct_shape(
+def test_if_null_df_with_stac_items_has_correct_shape(
     test_points_with_stac_null: gpd.GeoDataFrame, sample_test_data: gpd.GeoDataFrame
 ):
     assert len(test_points_with_stac_null) == len(sample_test_data)
 
 
-def test_if_stac_ids_from_test_null_df_are_empty(
+def test_if_stac_items_from_test_null_df_are_empty(
     test_points_with_stac_null: gpd.GeoDataFrame,
 ):
     assert test_points_with_stac_null["stac_item"].isnull().all()
 
 
-# -----Tests for stac ID fetching with seasonal data-----
-def test_if_seasonal_stac_ids_are_added_to_test_df(
+# -----Tests for stac item fetching with seasonal data-----
+@pytest.fixture(scope="module")
+def test_points_with_seasonal_stac(sample_test_data: gpd.GeoDataFrame):
+    """Sample test data with stac items."""
+    points_gdf = utl.df_w_latlons_to_gdf(sample_test_data)
+    return fetch_seasonal_stac_items(
+        points_gdf, "landsat-8-c2-l2", 2013, "planetary-compute"
+    )
+
+
+def test_if_seasonal_stac_items_are_added_to_test_df(
     test_points_with_seasonal_stac: gpd.GeoDataFrame,
 ):
     assert "stac_item" in test_points_with_seasonal_stac.columns and type(
@@ -94,7 +99,7 @@ def test_if_season_information_in_test_df_is_correct(
     ]
 
 
-def test_if_df_with_seasonal_stac_ids_has_correct_shape(
+def test_if_df_with_seasonal_stac_items_has_correct_shape(
     test_points_with_seasonal_stac: gpd.GeoDataFrame, sample_test_data: gpd.GeoDataFrame
 ):
     assert len(test_points_with_seasonal_stac) == 4 * len(sample_test_data)
@@ -106,3 +111,26 @@ def test_if_seasonal_stac_df_correctly_duplicates_points(
     assert np.all(test_points_with_seasonal_stac["Lat"].value_counts() == 4) and len(
         test_points_with_seasonal_stac["Lat"].value_counts()
     ) == len(sample_test_data)
+
+
+# -----Tests for stac item fetch from stac ID-----
+@pytest.fixture(scope="module")
+def stac_item_from_stac_id(sample_test_data: gpd.GeoDataFrame):
+    """Stac Item fetched from stac ID."""
+    stac_api = get_stac_api("planetary-compute")
+    id = stac_api.search(
+        collections=["landsat-8-c2-l2"],
+        intersects=sample_test_data["geometry"][0],
+        datetime=["2015-01-01", "2015-12-31"],
+        query={"eo:cloud_cover": {"lt": 10}},
+        limit=500,
+    ).item_collection_as_dict()["features"][0]["id"]
+    return fetch_stac_item_from_id(id, "planetary-compute")
+
+
+def test_if_stac_item_is_returned_from_id(stac_item_from_stac_id: Item):
+    assert type(stac_item_from_stac_id[0]) == Item
+
+
+def test_if_stac_item_list_has_correct_shape(stac_item_from_stac_id: Item):
+    assert len(stac_item_from_stac_id) == 1
