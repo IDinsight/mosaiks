a78664a064cf1067d8b370259a15ee52eaf5181b
poornimaramesh <poornimaramesh1995@gmail.com>
2023-06-09 15:05:29 +0100

    WIP: test for dask run functions

--- tests/test_dask_run.py
+++ tests/test_dask_run.py
@@ -1,26 +1,70 @@
 """Test dask code."""
+from os import listdir
+from pathlib import Path
+from shutil import rmtree
+from typing import Generator
+
 import numpy as np
 import pandas as pd
 import pytest
 from dask.delayed import Delayed
+from dask.distributed import Client
 
-from mosaiks.dask import delayed_partition_run
-from mosaiks.featurize import RCF, fetch_image_refs, get_dask_gdf
-from mosaiks.utils import df_w_latlons_to_gdf
+from mosaiks import utils as utl
+from mosaiks.dask import (
+    get_local_dask_client,
+    get_partitions_generator,
+    run_batched_delayed_pipeline,
+    run_queued_futures_pipeline,
+    run_unbatched_delayed_pipeline,
+)
+from mosaiks.featurize import RCF
+
+
+# -----Test partitions generator-----
+@pytest.fixture(scope="module")
+def partitions_generator(sample_test_data: pd.DataFrame):
+    """Return partitions generator."""
+    test_points_gdf = utl.df_w_latlons_to_gdf(sample_test_data)
+    return get_partitions_generator(test_points_gdf, 5)
+
+
+def test_if_get_partitions_generator_returns_generator(partitions_generator: Generator):
+    assert isinstance(partitions_generator, Generator)
+
+
+def test_if_get_partitions_generator_returns_correct_number_of_partitions(
+    partitions_generator: Generator,
+):
+    assert len(list(partitions_generator)) == 2
+
+
+def test_if_get_partitions_generator_returns_correct_type_of_partitions(
+    partitions_generator: Generator,
+):
+    assert isinstance(next(partitions_generator), pd.DataFrame)
+
+
+def test_if_get_partitions_generator_returns_correct_number_of_points_in_partitions(
+    partitions_generator: Generator,
+):
+    assert len(next(partitions_generator)) == 5
+
+
+# -----Test local dask client-----
+@pytest.fixture(scope="module")
+def client():
+    return get_local_dask_client()
 
 
 @pytest.mark.slow
-def test_delayed_partition_run(
-    featurization_params: dict, satellite_config: dict, sample_test_data: pd.DataFrame
+def test_run_queued_futures(
+    sample_test_data: pd.DataFrame,
+    client: Client,
+    featurization_params: dict,
+    satellite_config: dict,
 ):
-    """Test delayed partition run."""
-    points_gdf = df_w_latlons_to_gdf(sample_test_data)
-    points_dgdf = get_dask_gdf(points_gdf, 10)
-    points_gdf_w_stac = fetch_image_refs(
-        points_dgdf, featurization_params["satellite_search_params"]
-    )
-
-    partitions = points_gdf_w_stac.to_delayed()
+    points_gdf = utl.df_w_latlons_to_gdf(sample_test_data)
     model = RCF(
         featurization_params["model"]["num_features"],
         featurization_params["model"]["kernel_size"],
@@ -29,10 +73,76 @@ def test_delayed_partition_run(
     columns = [
         "feature_%d" % i for i in range(featurization_params["model"]["num_features"])
     ]
-    run = delayed_partition_run(
-        partitions[0], satellite_config, featurization_params, columns, model
+    folder_path = "tests/data/test_output/"
+    run_queued_futures_pipeline(
+        points_gdf,
+        client,
+        model,
+        featurization_params,
+        satellite_config,
+        columns,
+        folder_path,
     )
-    df = run.compute()
-    assert isinstance(run, Delayed)
-    assert df.shape == (len(points_gdf), featurization_params["model"]["num_features"])
-    assert np.all(df.columns.tolist() == columns)
+    assert len(listdir(folder_path)) == 2
+    rmtree(folder_path)
+
+
+@pytest.mark.slow
+def test_run_batched_delayed_pipeline(
+    sample_test_data: pd.DataFrame,
+    client: Client,
+    featurization_params: dict,
+    satellite_config: dict,
+):
+    points_gdf = utl.df_w_latlons_to_gdf(sample_test_data)
+    model = RCF(
+        featurization_params["model"]["num_features"],
+        featurization_params["model"]["kernel_size"],
+        len(satellite_config["bands"]),
+    )
+    columns = [
+        "feature_%d" % i for i in range(featurization_params["model"]["num_features"])
+    ]
+    folder_path = "tests/data/test_output/"
+    run_batched_delayed_pipeline(
+        points_gdf,
+        client,
+        model,
+        featurization_params,
+        satellite_config,
+        columns,
+        folder_path,
+    )
+    assert len(listdir(folder_path)) == 2
+    rmtree(folder_path)
+
+
+@pytest.mark.slow
+def test_run_unbatched_delayed_pipeline(
+    sample_test_data: pd.DataFrame,
+    client: Client,
+    featurization_params: dict,
+    satellite_config: dict,
+):
+    points_gdf = utl.df_w_latlons_to_gdf(sample_test_data)
+    model = RCF(
+        featurization_params["model"]["num_features"],
+        featurization_params["model"]["kernel_size"],
+        len(satellite_config["bands"]),
+    )
+    columns = [
+        "feature_%d" % i for i in range(featurization_params["model"]["num_features"])
+    ]
+    folder_path = Path("tests/data/test_output/")
+    folder_path.mkdir(parents=True, exist_ok=True)
+    run_unbatched_delayed_pipeline(
+        points_gdf,
+        client,
+        model,
+        featurization_params,
+        satellite_config,
+        columns,
+        folder_path,
+    )
+    assert len(listdir(folder_path)) == 2
+    rmtree(folder_path)
