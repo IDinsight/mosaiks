1cc3b1744b1194130087ab93dd12c7f7c36b442e
Poornima Ramesh <poornimaramesh1995@gmail.com>
2023-07-12 09:31:53 +0300

    set manual seed to initialise conv filters same way (#139)
    * set manual seed to initialise conv filters same way
    
    * update to make random seed a config param
    
    * remove the now unnecessary initial torch seed
    
    ---------
    
    Co-authored-by: amir_emami <amirali1376@gmail.com>

--- tests/test_featurize.py
+++ tests/test_featurize.py
@@ -8,7 +8,23 @@ from mosaiks.featurize import RCF
 @pytest.fixture(scope="module")
 def model():
     """Fixture for a random convolutional features model."""
-    return RCF(num_features=666, kernel_size=5, num_input_channels=3)
+    return RCF(
+        num_features=666,
+        kernel_size=5,
+        num_input_channels=3,
+        random_seed_for_filters=42,
+    )
+
+
+@pytest.fixture(scope="module")
+def model2():
+    """Fixture for a random convolutional features model."""
+    return RCF(
+        num_features=666,
+        kernel_size=5,
+        num_input_channels=3,
+        random_seed_for_filters=42,
+    )
 
 
 def test_random_model_conv_weights_are_created(model):
@@ -20,6 +36,10 @@ def test_random_model_conv_weights_are_not_trainable(model):
         model.conv1.weight.requires_grad == False
         and model.conv1.bias.requires_grad == False
     )
+
+
+def test_random_model_conv_weights_are_always_initialised_to_same_values(model, model2):
+    assert (model.conv1.weight == model2.conv1.weight).all().item()
 
 
 def test_random_conv_features_are_created(model):
