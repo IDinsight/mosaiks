9d7971c5d105e15f08e23df49e5e3c904502b2b6
poornimaramesh <poornimaramesh1995@gmail.com>
2023-07-14 16:47:51 +0300

    update I/O method to also take datetime

--- src/mosaiks/extras.py
+++ src/mosaiks/extras.py
@@ -9,6 +9,7 @@ def load_and_save_features(
     path_to_save_data: str,
     lat_col: str,
     lon_col: str,
+    datetime: str or list[str] or callable,
     context_cols_to_keep_from_input: list = None,
     **kwargs,
 ) -> None:
@@ -21,6 +22,9 @@ def load_and_save_features(
     path_to_save_data : Path to save data, in either .csv or .parquet format.
     lat_col : Name of latitude column in input data
     lon_col : Name of longitude column in input data
+    datetime: date/times for fetching satellite images. See the STAC API documentation
+        (https://pystac-client.readthedocs.io/en/latest/api.html#pystac_client.Client)
+        for `.search`'s `datetime` parameter for more details
     context_cols_to_keep_from_input : List of context columns to add to final dataframe from input data
     kwargs: config parameters for `get_features`. See `get_features` docstring for more details and default values.
     """
@@ -28,7 +32,9 @@ def load_and_save_features(
     points_df = utl.load_dataframe(input_file_path)
 
     # Get features
-    features_df = get_features(points_df[lat_col], points_df[lon_col], **kwargs)
+    features_df = get_features(
+        points_df[lat_col], points_df[lon_col], datetime, **kwargs
+    )
 
     # Add context columns
     if context_cols_to_keep_from_input:
